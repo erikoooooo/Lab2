39fb9b22bee4c073267f867cf6e032dccbeade19
eriko <erikolim.21@ichat.sp.edu.sg>
2022-11-03 12:00:34 +0800

    Initial version to display text message on console

--- Lab2.py
+++ Lab2.py
@@ -1,18 +1,3 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
 
-def calculate_bmi(height, weight):
-    print("height = " + str(height))
-    print("weight = " + str(weight))
-    bmi = weight / (height * height)
-    print("bmi = " + str(bmi))
-
-    if(bmi < 18.5):
-        print("under weight")
-    elif(bmi >= 18.5 and bmi <= 25.0):
-        print("normal weight")
-    else:
-        print("over weight")
-
-calculate_bmi(weight=57, height=1.73)
-
 
